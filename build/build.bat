274cb7873951b6580bb1d3560a0b15a11be84577
Sn3ppi <racers.340@gmail.com>
2024-12-21 18:42:02 +0300

    fix: CI/CD requirements.txt path fix: build.bat requirements.txt path

--- build/build.bat
+++ build/build.bat
@@ -8,7 +8,7 @@ cd AppBuild
     move ffmpeg-7.1-full_build ffmpeg
     python -m venv pkgs
     call pkgs\Scripts\activate
-        pip install -r "..build\requirements.txt"
+        pip install -r "..\build\requirements.txt"
     call pkgs\Scripts\deactivate
     pyinstaller -F --name "YTPlaylistParser" --paths="pkgs/Lib/Site-packages;." --add-data "ffmpeg;ffmpeg" "..\src\main.py"
 cd ..
